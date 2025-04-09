04568d8a2af11b0a399328a69e9a8d80ddf64c0a
Ruslan Zan <reaasre@gmail.com>
2025-04-09 18:29:40 +0300

    plot image upload fix

--- backend/plots/models.py
+++ backend/plots/models.py
@@ -13,7 +13,7 @@ class Plot(models.Model):
     description = models.TextField()
     code = models.TextField()
     likes = models.IntegerField(default=0)
-    main_image = models.ImageField(blank=True, upload_to='plot_images', default='default_plot.png')
+    main_image = models.ImageField(blank=True, upload_to='plot_images/', default='default_plot.png')
     
 
     def get_absolute_url(self):
